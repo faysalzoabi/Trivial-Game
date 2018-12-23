2b887624cb818ec11dc52528fb08a8f1b7f08a81
Faysal AlZoubi <faysal.zoabi@gmail.com>
2018-12-24 00:16:45 +0100

    Updates

---
+++ precache-manifest.6df9f0e4a2efed1ee9c16c15f88b3cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b8a71a2250d96c5e35d",
+    "url": "/Trivial-Game/static/js/runtime~main.3b8a71a2.js"
+  },
+  {
+    "revision": "71f6e04902110b8f0887",
+    "url": "/Trivial-Game/static/js/main.71f6e049.chunk.js"
+  },
+  {
+    "revision": "d1946c4cfbfd05ca861d",
+    "url": "/Trivial-Game/static/js/1.d1946c4c.chunk.js"
+  },
+  {
+    "revision": "71f6e04902110b8f0887",
+    "url": "/Trivial-Game/static/css/main.2434a2ae.chunk.css"
+  },
+  {
+    "revision": "ac4b7425b73fcda51b4290e798a81c63",
+    "url": "/Trivial-Game/index.html"
+  }
+];
